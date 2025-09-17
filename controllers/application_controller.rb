5b0348cabf0d1b48d291820dcad0b0f5239f3f3b
RockyLatchman <r.latchman3624@gmail.com>
2025-09-17 14:32:57 +0700

    Added 404 not_found template and route

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < Sinatra::Base
     set :views, File.expand_path('../../views', __FILE__)
   end
 
+  not_found do
+    erb :not_found
+  end
+
   get '/' do
     erb :index
   end
