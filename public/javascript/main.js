1c4c8be1a7378a151e6c79005e3e5850b5519e8d
RockyLatchman <r.latchman3624@gmail.com>
2025-10-09 10:43:16 +0700

    CSS for add staff modal on Admin desk template

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -21,6 +21,31 @@ const modal = `
       margin: 10% auto 0 auto;
   }
 
+  .modal input[type='file']::placeholder {
+    text-indent: -100000px;
+  }
+
+
+
+  .modal h2 {
+    font: 900 1.2em "Cousine", sans-serif;
+    color: var(--accentColor);
+    margin: 25px 0 5px 15px;
+  }
+
+  .modal label {
+    display: block;
+    font: 100 0.5em "Cousine", sans-serif;
+    color: var(--formFieldTextColor);
+    margin: 5px 0 25px 0;
+    border: 1px solid #E4E4E4;
+    border-radius: 5px;
+    width: 100%;
+    height: 35px;
+    padding: 3px 0 0 5px;
+    background: #fff;
+  }
+
   .confirm-modal {
      width: 25%;
      height: 150px;
